0fa2ac3907fe31c1b338fbf5b84f96a88b385e35
Yurii <stejlyura19@gmail.com>
2026-01-13 10:42:47 +0200

    Add elecrton feature

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
 })
