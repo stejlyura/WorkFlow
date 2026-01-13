4eededff360f04715293ea5e38d0251740ef6e10
Yurii <stejlyura19@gmail.com>
2026-01-13 11:04:43 +0200

    add tray adn new icon

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,15 +1,20 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
 const path = require("path");
 
 const isDev = !app.isPackaged;
 
-const Width = 1200
-const Height = 800
+let win;
+let tray;
+
+const Width = 1200;
+const Height = 800;
+const iconPath = path.join(__dirname, "..", "assets", "icon.ico");
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: Width,
     height: Height,
+    icon: iconPath,
     webPreferences: {
       preload: path.join(__dirname, "preload.cjs"),
       contextIsolation: true,
@@ -23,10 +28,53 @@ function createWindow() {
   } else {
     win.loadFile(path.join(__dirname, "..", "dist", "index.html"));
   }
+
+  win.on("close", (e) => {
+    if (!app.isQuiting) {
+      e.preventDefault();
+      win.hide();
+    }
+  });
+}
+
+function createTray() {
+  tray = new Tray(iconPath);
+  tray.setToolTip(app.getName());
+
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show",
+      click: () => {
+        if (win) {
+          win.show();
+          win.focus();
+        }
+      },
+    },
+    {
+      label: "Quit",
+      click: () => {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  tray.setContextMenu(contextMenu);
+  tray.on("click", () => {
+    if (!win) return;
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+      win.focus();
+    }
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
+  createTray();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
